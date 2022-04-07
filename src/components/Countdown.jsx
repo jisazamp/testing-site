23d355a01ba5c278d29ed9eed0ac55a230e41bc5
Juan P. Isaza <jisazamp@gmail.com>
2022-04-07 11:47:14 -0500

    Removed the rendering of the seconds

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,10 +1,10 @@
 import { useCountdown } from '../hooks/useCountdown';
 
 const Countdown = ({ targetDate, message }) => {
-  const [days, hours, minutes, seconds] = useCountdown(targetDate);
+  const [days, hours, minutes] = useCountdown(targetDate);
   return (
     <div className='ml-6 mr-6'>
-      <div className='text-white text-xl py-2 px-3 bg-gray-500 flex max-w-2xl text-center mt-5 rounded justify-center mx-auto font-semibold'>{`Faltan ${days} días, ${hours} horas, ${minutes} minutos y ${seconds} segundos ${message}`}</div>
+      <div className='text-white text-xl py-2 px-3 bg-gray-500 flex max-w-2xl text-center mt-5 rounded justify-center mx-auto font-semibold'>{`Faltan ${days} días, ${hours} horas, y ${minutes} minutos ${message}`}</div>
     </div>
   );
 };
